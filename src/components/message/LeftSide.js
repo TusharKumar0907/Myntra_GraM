f17be1ea213ad5a284f25ea8cf97e9ed154999f3
TUSHAR <tktusharkumar12345@gmail.com>
2023-08-07 19:24:56 +0530

    search user in message done

--- src/components/message/LeftSide.js
+++ src/components/message/LeftSide.js
@@ -1,38 +1,71 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { getDataAPI } from '../../utils/fetchData.js';
 import { GLOBALTYPES } from '../../redux/actions/globalTypes.js';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import UserCard from '../UserCard.js';
 
 const LeftSide = () => {
 
     const dispatch = useDispatch();
-    const { auth } = useState();
+    const { auth } = useSelector(state => state);
     
     const [search, setSearch] = useState('')
     const [searchUsers, setSearchUsers] = useState([])
 
-    const handleSearch = async e => {
-        e.preventDefault()
-        if(!search) return setSearchUsers([]);
-
-        try {
-            const res = await getDataAPI(`search?username=${search}`, auth.token)
-            setSearchUsers(res.data.users)
-        } catch (err) {
-            dispatch({
-                type: GLOBALTYPES.ALERT, payload: {error: err.response.data.msg}
-            })
-        }
+    const handleAddUser = (user) => {
+        console.log(user);
     }
+
+    useEffect(() => {
+        
+        if(search && auth.token) {
+            getDataAPI(`search?username=${ search }`, auth.token)
+            .then(res => {
+                console.log(res);
+                setSearchUsers(res.data.users)
+            })
+            .catch(err => {
+                dispatch({
+                    type: GLOBALTYPES.ALERT , payload: {error: err.response.data.msg}
+                });
+            })
+        } else {
+            setSearchUsers([]);
+        }
+    }, [search, auth.token, dispatch]);
+
+    // console.log(searchUsers);
 
     return (
        <>
-            <form className="message_header" onSubmit={handleSearch} >
+            <form className="message_header">
                 <input type="text" value={search}
                 placeholder="Enter to Search..."
                 onChange={e => setSearch(e.target.value)} />
                 <button type="submit" style={{display: 'none'}}>Search</button>
             </form>
+
+            <div className="message_chat_list">
+                
+                {
+                    
+                    searchUsers.length !== 0
+                    ? <>
+                    {   
+                        searchUsers.map(user => (
+                            <div key={user._id} className="message_user"
+                            onClick={() => handleAddUser(user)}>
+                                <UserCard user = {user} />
+                            </div>
+                        ))
+                    }
+                    </>
+                    : <>
+                    </>
+
+                }
+
+            </div>
 
 
        </>
